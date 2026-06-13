1ad691020e27a12c348a709c583722493c9a6b51
Shreyansh KUshwaha <shreyansh.kushw@gmail.com>
2026-06-13 23:39:49 +0530

    Changing the argument and return value types

--- app/services/embedder.py
+++ app/services/embedder.py
@@ -2,8 +2,8 @@ from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-def genenrate_embeddings(chunks:list[str]):
+def genenrate_embeddings(chunk:str):
 
     """Generates and returns a list of embeddings of all the chunks."""
 
-    return [model.encode(chunk) for chunk in chunks]
+    return model.encode(chunk)
