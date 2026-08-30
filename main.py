c7af0fa70dc2584af3ce6f12c24dafd85432e7dc
Shreyansh Kushwaha <shreyansh.kushw@gmail.com>
2026-08-30 19:53:20 +0530

    Incorporating S3 file upload logic

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from typing import Annotated
-from fastapi import BackgroundTasks, Depends, FastAPI, File, UploadFile
+from fastapi import BackgroundTasks, Depends, FastAPI, File, UploadFile, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 from sqlalchemy.ext.asyncio import AsyncSession
 from pydantic import Field
 import uuid
 
-from fastapi.middleware.cors import CORSMiddleware
 from app.services.pipelines import ingestion_pipeline, retrieval_pipeline
 from app.database import get_db
 from app.schema import QueryRequest
+from app.services.s3_client import s3, BUCKET_NAME
 
 app = FastAPI()
 
@@ -31,15 +32,45 @@ async def upload_file(
     job_id = str(uuid.uuid4().hex)  # creating random job ids.
 
     for index, file in enumerate(files):
-        filepath = Path(
-            f"upload_files/{job_id+str(index+1)}{Path(file.filename).suffix}"
-        )  # creating the relative filepath
+    #     filepath = Path(
+    #         f"upload_files/{job_id+str(index+1)}{Path(file.filename).suffix}"
+    #     )  # creating the relative filepath
 
-        with open(filepath, "wb") as f:
-            content = await file.read()  # reading the file content
-            f.write(content)  # copying the file content to another file
+    #     with open(filepath, "wb") as f:
+    #         content = await file.read()  # reading the file content
+    #         f.write(content)  # copying the file content to another file
 
-        background_tasks.add_task(ingestion_pipeline, filepath, db, job_id)
+    #     background_tasks.add_task(ingestion_pipeline, filepath, db, job_id)
+
+        try: 
+
+            s3_key = f"{job_id+str(index+1)}{Path(file.filename).suffix}"
+
+            s3.upload_fileobj(
+                file.file,
+                BUCKET_NAME,
+                s3_key,
+                ExtraArgs={
+                    "ContentType": file.content_type,
+                },
+            )
+
+            filename = Path(s3_key).name
+
+            with tempfile.TemporaryDirectory() as tmpdir:
+
+                local_path = Path(tmpdir) / filename
+
+                s3.download_file(
+                    BUCKET_NAME,
+                    s3_key,
+                    str(local_path),
+                )
+
+                background_tasks.add_task(ingestion_pipeline, local_path, db, job_id)
+
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
     return {"job_id": str(job_id), "message": "Files uploaded successfully"}
 
